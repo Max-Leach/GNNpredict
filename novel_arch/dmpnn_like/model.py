4a3d815aee84fdbde834cc7a5657494c5903d312
Preet Mistry <pmistry9597@gmail.com>
2023-05-25 12:30:29 -0400

    dmpnn_like initial

---
+++ novel_arch/dmpnn_like/model.py
@@ -0,0 +1,333 @@
+## this architecture will use largely bondnet's base
+# -however, the g2g module will be more D-MPNN-like (as in Grambow paper), 
+# where node features do not exist, and bonds are now represented by two directed edges in opposing directions
+# global state will remain, and will be updated after edge update
+# -g2p will transform the directed bond features into atom features for the readout, so 
+# it will operate on atom and global features
+
+# we may keep g2g module as same parameters for this model, so it will be applying
+# one d-mpnn update many times
+
+import itertools
+import dgl
+import torch
+from novel_arch.archic_0.model_core import GatedGCNMolCustomConv
+from novel_arch.archic_0.directed_graph_transform import to_directed_mpnn_g
+from novel_arch.archic_0.to_dmpnn_features import GrambowFeaturizer
+from novel_arch.archic_0.readout_feature_transform import DBondtoAtomFeaturize
+
+class GatedGCNReactionNetworkDMPNN(GatedGCNMolCustomConv):
+    def __init__(
+        self,
+        in_feats,
+        dbond_feat_size, # size from atom, bond -> directed edges
+        node_types, # types of nodes to use for processing from original graph
+
+        embedding_size=32,
+        gated_num_layers=2,
+        gated_hidden_size=[64, 64, 32],
+        gated_num_fc_layers=1,
+        gated_graph_norm=False,
+        gated_batch_norm=True,
+        gated_activation="ReLU",
+        gated_residual=True,
+        gated_dropout=0.0,
+        num_lstm_iters=6,
+        num_lstm_layers=3,
+        set2set_ntypes_direct=["global"],
+        fc_num_layers=2,
+        fc_hidden_size=[32, 16],
+        fc_batch_norm=False,
+        fc_activation="ReLU",
+        fc_dropout=0.0,
+        outdim=1,
+        conv_op = None,
+    ):
+        # feature size so embedding (from d_bond raw to hidden) can properly process
+        dmpnn_feats = {'d_bond': dbond_feat_size, 'global': in_feats['global']}
+    
+        super().__init__(
+            dmpnn_feats,
+            embedding_size,
+            gated_num_layers,
+            gated_hidden_size,
+            gated_num_fc_layers,
+            gated_graph_norm,
+            gated_batch_norm,
+            gated_activation,
+            gated_residual,
+            gated_dropout,
+            num_lstm_iters,
+            num_lstm_layers,
+            set2set_ntypes_direct,
+            fc_num_layers,
+            fc_hidden_size,
+            fc_batch_norm,
+            fc_activation,
+            fc_dropout,
+            outdim,
+            conv_op=conv_op,
+        )
+
+        self.graph_featurizer = GrambowFeaturizer(in_feats['atom'], in_feats['bond'], dbond_feat_size)
+        self.node_types = node_types
+        self.prereadout_featurizer = DBondtoAtomFeaturize(in_feats['atom'], gated_hidden_size[-1], gated_hidden_size[-1])
+        self.in_feats = in_feats
+
+    def forward(self, graph, feats, reactions, norm_atom=None, norm_bond=None):
+        """
+        Args:
+            graph (DGLHeteroGraph or BatchedDGLHeteroGraph): (batched) molecule graphs
+            feats (dict): node features with node type as key and the corresponding
+                features as value.
+            reactions (list): a sequence of :class:`bondnet.data.reaction_network.Reaction`,
+                each representing a reaction.
+            norm_atom (2D tensor or None): graph norm for atom
+            norm_bond (2D tensor or None): graph norm for bond
+
+        Returns:
+            2D tensor: of shape(N, M), where `M = outdim`.
+        """
+
+        # handle single atom case: bond gets removed
+        def single_atom_case(g):
+            if g.num_nodes('atom') == 1:
+                bonds = range(g.num_nodes('bond'))
+                return dgl.remove_nodes(g, bonds, ntype='bond')
+            else:
+                return g
+
+        # dmpnn style transform
+        # NOTE: likely should move this outside of forward: does transform
+        # # on all molecules every single time!
+        original_feats = {nt: graph.nodes[nt].data["feat"] for nt in self.node_types}
+        original_graph = graph
+        graphs = dgl.unbatch(graph)
+        graphs = tuple(map(single_atom_case, graphs))
+        graph = dgl.batch(graphs)
+        graph = to_directed_mpnn_g(graph)
+
+        # original features to dmpnn features
+        feats = self.graph_featurizer(feats, graph)
+
+        # embedding
+        feats = self.embedding(feats)
+
+        # gated layer
+        for layer in self.gated_layers:
+            feats = layer(graph, feats, norm_atom, norm_bond)
+
+        # reverse dmpnn to atom + global features
+        feats = self.prereadout_featurizer(feats, graph, original_feats)
+
+        # convert mol graphs to reaction graphs by subtracting reactant feats from
+        # products feats
+        graph, feats = mol_graph_to_rxn_graph(original_graph, feats, reactions)
+
+        # readout layer
+        feats = self.readout_layer(graph, feats)
+
+        # fc
+        for layer in self.fc_layers:
+            feats = layer(feats)
+
+        return feats
+
+    def feature_before_fc(self, graph, feats, reactions, norm_atom, norm_bond):
+        """
+        Get the features before the final fully-connected.
+
+        This is used for feature visualization.
+        """
+        # embedding
+        feats = self.embedding(feats)
+
+        # gated layer
+        for layer in self.gated_layers:
+            feats = layer(graph, feats, norm_atom, norm_bond)
+
+        # convert mol graphs to reaction graphs by subtracting reactant feats from
+        # products feats
+        graph, feats = mol_graph_to_rxn_graph(graph, feats, reactions)
+
+        # readout layer
+        feats = self.readout_layer(graph, feats)
+
+        return feats
+
+    def feature_at_each_layer(self, graph, feats, reactions, norm_atom, norm_bond):
+        """
+        Get the features at each layer before the final fully-connected layer.
+
+        This is used for feature visualization to see how the model learns.
+
+        Returns:
+            dict: (layer_idx, feats), each feats is a list of
+        """
+
+        layer_idx = 0
+        all_feats = dict()
+
+        # embedding
+        feats = self.embedding(feats)
+
+        # store bond feature of each molecule
+        fts = _split_batched_output(graph, feats["bond"])
+        all_feats[layer_idx] = fts
+        layer_idx += 1
+
+        # gated layer
+        for layer in self.gated_layers:
+            feats = layer(graph, feats, norm_atom, norm_bond)
+
+            # store bond feature of each molecule
+            fts = _split_batched_output(graph, feats["bond"])
+            all_feats[layer_idx] = fts
+            layer_idx += 1
+
+        return all_feats
+
+
+# def _split_batched_output(graph, value):
+#     """
+#     Split a tensor into `num_graphs` chunks, the size of each chunk equals the
+#     number of bonds in the graph.
+
+#     Returns:
+#         list of tensor.
+
+#     """
+#     nbonds = graph.batch_num_nodes("bond")
+#     return torch.split(value, nbonds)
+
+
+def mol_graph_to_rxn_graph(graph, feats, reactions):
+    """
+    Convert a batched molecule graph to a batched reaction graph.
+
+    Essentially, a reaction graph has the same graph structure as the reactant and
+    its features are the difference between the products features and reactant features.
+
+    Args:
+        graph (BatchedDGLHeteroGraph): batched graph representing molecules.
+        feats (dict): node features with node type as key and the corresponding
+            features as value.
+        reactions (list): a sequence of :class:`bondnet.data.reaction_network.Reaction`,
+            each representing a reaction.
+
+    Returns:
+        batched_graph (BatchedDGLHeteroGraph): a batched graph representing a set of
+            reactions.
+        feats (dict): features for the batched graph
+    """
+    # TODO add graph.local_var() since hetero and homo graphs are combined
+    # should not use graph.local_var() to make a local copy, since it converts a
+    # BatchedDGLHeteroGraph into a DGLHeteroGraph. Then unbatch_hetero(graph) below
+    # will not work.
+    # If you really want to, use copy.deepcopy() to make a local copy
+
+    # assign feats
+    for nt, ft in feats.items():
+        graph.nodes[nt].data.update({"ft": ft})
+
+    # unbatch molecule graph
+    graphs = dgl.unbatch(graph)
+
+    # create reaction graphs
+    reaction_graphs = []
+    reaction_feats = []
+    for rxn in reactions:
+        reactants = [graphs[i] for i in rxn.reactants]
+        products = [graphs[i] for i in rxn.products]
+
+        # whether a molecule has bonds?
+        has_bonds = {
+            # we support only one reactant now, so no it is assumed always to have bond
+            "reactants": [True for _ in reactants],
+            "products": [True if len(mp) > 0 else False for mp in rxn.bond_mapping],
+        }
+        mappings = {"atom": rxn.atom_mapping_as_list}#, "bond": rxn.bond_mapping_as_list}
+
+        g, fts = create_rxn_graph(
+            reactants, products, mappings, has_bonds, tuple(feats.keys())
+        )
+        reaction_graphs.append(g)
+        reaction_feats.append(fts)
+
+    # batched reaction graph and data
+    batched_graph = dgl.batch(reaction_graphs)
+    batched_feats = {}
+    for nt in feats:
+        batched_feats[nt] = torch.cat([ft[nt] for ft in reaction_feats])
+
+    return batched_graph, batched_feats
+
+
+def create_rxn_graph(
+    reactants,
+    products,
+    mappings,
+    has_bonds,
+    ntypes=("atom", "bond", "global"),
+    ft_name="ft",
+):
+    """
+    A reaction is represented by:
+
+    feats of products - feats of reactant
+
+    Args:
+        reactants (list of DGLHeteroGraph): a sequence of reactants graphs
+        products (list of DGLHeteroGraph): a sequence of product graphs
+        mappings (dict): with node type as the key (e.g. `atom` and `bond`) and a list
+            as value, which is a mapping between reactant feature and product feature
+            of the same atom (bond).
+        has_bonds (dict): whether the reactants and products have bonds.
+        ntypes (list): node types of which the feature are manipulated
+        ft_name (str): key of feature inf data dict
+
+    Returns:
+        graph (DGLHeteroGraph): a reaction graph with feats constructed from between
+            reactant and products.
+        feats (dict): features of reaction graph
+    """
+    assert len(reactants) == 1, f"number of reactants ({len(reactants)}) not supported"
+
+    # note, this assumes we have one reactant
+    graph = reactants[0]
+
+    feats = dict()
+    for nt in ntypes:
+        reactants_ft = [p.nodes[nt].data[ft_name] for p in reactants]
+        products_ft = [p.nodes[nt].data[ft_name] for p in products]
+
+        # remove bond ft if the corresponding molecule has no bond
+        # this is necessary because, to make heterogeneous graph work, we create
+        # factitious bond features for molecule without any bond (i.e. single atom
+        # molecule, e.g. H+)
+        if nt == "bond":
+            # select the ones that has bond
+            # note, this assumes only one bond missing in products
+            ## reactants_ft = list(itertools.compress(reactants_ft, has_bonds["reactants"]))
+            products_ft = list(itertools.compress(products_ft, has_bonds["products"]))
+
+            # add a feature with all zeros for the broken bond
+            products_ft.append(reactants_ft[0].new_zeros((1, reactants_ft[0].shape[1])))
+
+        reactants_ft = torch.cat(reactants_ft)
+        products_ft = torch.cat(products_ft)
+
+        if nt == "global":
+            reactants_ft = torch.sum(reactants_ft, dim=0, keepdim=True)
+            products_ft = torch.sum(products_ft, dim=0, keepdim=True)
+        else:
+            # reorder products_ft such that atoms (bonds) have the same order as reactants
+            assert len(products_ft) == len(mappings[nt]), (
+                f"products_ft ({len(products_ft)}) and mappings[{nt}] "
+                f"({len(mappings[nt])}) have different length"
+            )
+            products_ft = products_ft[mappings[nt]]
+
+        feats[nt] = products_ft - reactants_ft
+
+    return graph, feats
---
+++ novel_arch/dmpnn_like/to_dmpnn_features.py
@@ -0,0 +1,39 @@
+# feature transforms for bondnet graph to archic 0 type input
+from torch import nn
+import torch
+
+# exactly the same as in Grambow paper: bond, atom -> directed bond feature
+# but now global features are simply maintained
+# -for d_bond feat row, does relu(W*(hv||evw))
+class GrambowFeaturizer(nn.Module):
+    def __init__(self, atom_feat_size, bond_feat_size, out_feat_size):
+        super().__init__()
+
+        self.relu = nn.ReLU()
+        combined_size = atom_feat_size + bond_feat_size
+        self.map = nn.Linear(combined_size, out_feat_size)
+
+    def forward(self, feats, dmpnn_g):
+        dbond_precurs = gen_directed_bond_precurs(feats, dmpnn_g)
+        d_bond_feats = self.relu(self.map(dbond_precurs))
+
+        feats = {
+            'global': feats['global'],
+            'd_bond': d_bond_feats,
+        }
+        return feats
+
+# construct directed bond precursor matrix - each row is for each d bond associated atom and old bond features from bondnet style graph
+def gen_directed_bond_precurs(feats, dmpnn_g):
+    assert len(feats['atom']) > 0 and len(feats['bond']) > 0
+    atom_feat_size = len(feats['atom'][0])
+    bond_feat_size = len(feats['bond'][0])
+    # in place insertion
+    dbond_precurs = torch.empty([dmpnn_g.num_nodes('d_bond'), atom_feat_size + bond_feat_size])
+
+    for db in range(len(dbond_precurs)):
+        src_atom = dmpnn_g.nodes['d_bond'].data['src_atom'][db]
+        old_bond = dmpnn_g.nodes['d_bond'].data['old_bond'][db]
+        dbond_precurs[db] = torch.cat([feats['atom'][src_atom], feats['bond'][old_bond]])
+
+    return dbond_precurs
