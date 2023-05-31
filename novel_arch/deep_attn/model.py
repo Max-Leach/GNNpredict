522990b73cfcafcf4bd382d93a055c9f6bec1039
Preet Mistry <pmistry9597@gmail.com>
2023-05-31 12:39:33 -0400

    graph processing

--- novel_arch/deep_attn/model.py
+++ novel_arch/deep_attn/model.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torch
+from itertools import pairwise
 
 from novel_arch.deep_attn.feat_evolve import OrderedGraphFeatUpdate
 from novel_arch.deep_attn.state_mlp import ConcatStateMLP
@@ -8,26 +9,48 @@ from novel_arch.deep_attn.feat_type_updaters import concat_sum_atom_edge_feat, a
 
 class DeepAtomSum(nn.Module): ### NOTE: we may use a custom aggregator for this class for atom update
     ''' deeper state evolution, just add nearby atoms + edges for atom feat update '''
-    def __init__(self, in_feat_sizes):
+    def __init__(self, in_feat_sizes, embedding_size, graph_hidden_size, graph_layers, residual=True):
         super().__init__()
 
-        feat_updaters = {
-            'bond' : EdgeNeighborUpdate(in_feat_sizes),
-            'atom' : AtomAggregUpdate(concat_sum_atom_edge_feat, in_feat_sizes),
-            'global' : GlobalAggregUpdate(bond_mean(), atom_mean(), in_feat_sizes),
-        }
-        self.graph_layer_0 = OrderedGraphFeatUpdate(['bond', 'atom', 'global'], feat_updaters)
+        self.embedders = {k : nn.Linear(in_feat_sizes[k], embedding_size) for k in in_feat_sizes}
+        embed_feat_sizes = {'bond': embedding_size, 'atom': embedding_size, 'global': embedding_size}
+        feat_sizes = [embed_feat_sizes] + graph_layers * [graph_hidden_size]
+
+        graph_net = []
+        for in_s, out_s in pairwise(feat_sizes): # this for loop is to deal with case that hidden sizes are different, otherwise its more complicated than needed
+            in_feat_sizes = in_s
+            if not isinstance(in_feat_sizes, dict):
+                in_feat_sizes = {'bond': in_s, 'atom': in_s, 'global': in_s} # unified size for rest of graph layers
+            bond_updt = EdgeNeighborUpdate(in_feat_sizes, out_s, residual=residual)
+            in_feat_sizes['bond'] = out_s
+            atom_updt = AtomAggregUpdate(concat_sum_atom_edge_feat, in_feat_sizes, out_s, residual=residual)
+            in_feat_sizes['atom'] = out_s
+            global_updt = GlobalAggregUpdate(bond_mean(), atom_mean(), in_feat_sizes, out_s, residual=residual)
+
+            feat_updaters = {
+                'bond' : bond_updt,
+                'atom' : atom_updt,
+                'global' : global_updt,
+            }
+            graff_layer = OrderedGraphFeatUpdate(['bond', 'atom', 'global'], feat_updaters)
+            graph_net.append(graff_layer)
+        self.graph_net = graph_net
 
     def forward(self, feats, graph):
         g = graph.local_var()
         ## graph processing network
         #load feats to graph
-        for fname in feats:
-            g.nodes[fname].data.update({'ft': feats[fname]})
-        feats = self.graph_layer_0(feats, g)
-        return feats
+        # for fname in feats:
+        #     g.nodes[fname].data.update({'ft': feats[fname]})
+        # feats = self.graph_net(feats, g)
+        for ftype in self.embedders: # unify all sizes
+            feats[ftype] = self.embedders[ftype](feats[ftype])
+        for gl in self.graph_net:
+            feats = gl(feats, graph)
 
         ## reaction graph construction via difference of component graphs
+        
+        return feats
 
         ## difference of reactant and product features to get reaction graph features
 
