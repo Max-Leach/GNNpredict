gradient fix, include edges option in atom aggregation

novel_arch/deep_attn/feat_type_updaters.py
@@ -41,13 +41,16 @@ class AtomAggregUpdate(nn.Module):
     '''
         custom aggregation of node features (especially bonds)
     '''
-    def __init__(self, atom_edge_feat_aggreg, in_feat_sizes, out_size, inner_layer_sizes=[], residual=False, bias=False):
+    def __init__(self, atom_edge_feat_aggreg, in_feat_sizes, out_size, inner_layer_sizes=[], residual=False, bias=False, include_edges=True):
         super().__init__()
 
         self.atom_edge_feat_aggreg = atom_edge_feat_aggreg
         self.residual = residual
-        in_mlp_size = 2*in_feat_sizes['atom']+in_feat_sizes['global']+in_feat_sizes['bond']
+        in_mlp_size = 2*in_feat_sizes['atom'] + in_feat_sizes['global']
+        if include_edges:
+            in_mlp_size += in_feat_sizes['bond']
         self.fc = mlp_from_sizes(in_mlp_size, out_size, inner_layer_sizes, bias=bias, batch_norm=True)
+        self.include_edges = include_edges
 
     def forward(self, feats, graph):
         g = graph.local_var()
@@ -80,13 +83,14 @@ def concat_sum_atom_edge_feat(nodes):
     atm, it uses gatv2 style attention
 '''
 class AttnAtomEdgeReducer(nn.Module):
-    def __init__(self, feat_size, internal_attn_size):
+    def __init__(self, feat_size, internal_attn_size, include_edges=True):
         super().__init__()
 
         self.activ_in_map = nn.Linear(2*feat_size + feat_size, internal_attn_size)
         self.activ = nn.LeakyReLU()
         self.attn_scalar_map = nn.Linear(internal_attn_size, 1)
         self.softmax = nn.Softmax()
+        self.include_edges = include_edges
 
     def forward(self, incoming_atom_fts, incoming_bond_fts, nodes):
         self_feats_per_incoming = nodes.data['ft'].unsqueeze(1).repeat_interleave(incoming_atom_fts.size(1), dim=1)
@@ -96,7 +100,9 @@ class AttnAtomEdgeReducer(nn.Module):
         pre_attn = self.attn_scalar_map(activ_ins)
         attn_weights = self.softmax(pre_attn)
 
-        incoming = torch.cat([incoming_atom_fts, incoming_bond_fts], dim=-1)
+        incoming = incoming_atom_fts
+        if self.include_edges:
+            incoming = torch.cat([incoming_atom_fts, incoming_bond_fts], dim=-1)
         weighted = torch.mul(attn_weights, incoming)
         aggregated = torch.sum(weighted, dim=1)
 

novel_arch/deep_attn/model.py
@@ -13,13 +13,13 @@ from bondnet.layer.readout import Set2SetThenCat
 class DeepAtom(nn.Module):
     ''' deeper state evolution, just add nearby atoms + edges for atom feat update '''
     ''' graph_inner_layer_sizes - how wide individual layers in gnn portion will be, is independent of graph_layers count '''
-    def __init__(self, atom_aggregators, in_feat_sizes, graph_hidden_size, graph_layers, graph_inner_layer_sizes=[], residual=True, fc_readout_sizes=[128, 64], set2set_iters=6, set2set_layers=3):
+    def __init__(self, atom_aggregators, in_feat_sizes, graph_hidden_size, graph_layers, graph_inner_layer_sizes=[], residual=True, fc_readout_sizes=[128, 64], set2set_iters=6, set2set_layers=3, atom_include_edges=True):
         super().__init__()
 
         embedding_size = graph_hidden_size
         self.embedders = {k : nn.Linear(in_feat_sizes[k], embedding_size) for k in in_feat_sizes}
         self.assemble_gnn(
-            in_feat_sizes, embedding_size, graph_hidden_size, graph_layers, graph_inner_layer_sizes, residual, atom_aggregators
+            in_feat_sizes, embedding_size, graph_hidden_size, graph_layers, graph_inner_layer_sizes, residual, atom_aggregators, atom_include_edges
             )
 
         ntypes = ["atom", "bond"]
@@ -38,7 +38,7 @@ class DeepAtom(nn.Module):
             in_size = out_size
         self.fc_to_scalar.append(nn.Linear(in_size, 1))
     
-    def assemble_gnn(self, in_feat_sizes, embedding_size, graph_hidden_size, graph_layers, graph_inner_layer_sizes, residual, atom_aggregators):
+    def assemble_gnn(self, in_feat_sizes, embedding_size, graph_hidden_size, graph_layers, graph_inner_layer_sizes, residual, atom_aggregators, atom_include_edges):
         embed_feat_sizes = {'bond': embedding_size, 'atom': embedding_size, 'global': embedding_size}
         feat_sizes = [embed_feat_sizes] + graph_layers * [graph_hidden_size]
 
@@ -56,15 +56,15 @@ class DeepAtom(nn.Module):
                 atom_aggregator = atom_aggregators
             bond_updt = EdgeNeighborUpdate(in_feat_sizes, out_s, inner_layer_sizes=inner_layer_sizes, residual=residual)
             in_feat_sizes['bond'] = out_s
-            atom_updt = AtomAggregUpdate(atom_aggregator, in_feat_sizes, out_s, inner_layer_sizes=inner_layer_sizes, residual=residual)
+            atom_updt = AtomAggregUpdate(atom_aggregator, in_feat_sizes, out_s, inner_layer_sizes=inner_layer_sizes, residual=residual, include_edges=atom_include_edges)
             in_feat_sizes['atom'] = out_s
             global_updt = GlobalAggregUpdate(bond_mean(), atom_mean(), in_feat_sizes, out_s, inner_layer_sizes=inner_layer_sizes, residual=residual)
 
-            feat_updaters = {
+            feat_updaters = nn.ModuleDict({
                 'bond' : bond_updt,
                 'atom' : atom_updt,
                 'global' : global_updt,
-            }
+            })
             graff_layer = OrderedGraphFeatUpdate(['bond', 'atom', 'global'], feat_updaters)
             graph_net.append(graff_layer)
         self.graph_net = graph_net