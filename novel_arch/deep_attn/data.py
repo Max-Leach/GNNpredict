cfed82e154ac79d2cb080af8353e6cea07fec9fb
Preet Mistry <pmistry9597@gmail.com>
2023-06-19 17:39:48 -0400

    mapping preliminary

--- novel_arch/deep_attn/data.py
+++ novel_arch/deep_attn/data.py
@@ -1,16 +1,17 @@
 from rdkit import Chem
 import dgl
 import torch
+import networkx.algorithms.isomorphism as nx_iso
 
 ''' 
-        end storage should help create at end: 
-        all unique molecules stored, 
-        associated features for all, 
-        cover items for BondDissociate instance:
-            mappings b/w reactant, products features concated
+    end storage should help create at end: 
+    all unique molecules stored, 
+    associated features for all, 
+    cover items for BondDissociate instance:
+        mappings b/w reactant, products features concated
 
-        final dataset should batch required graphs, get batched features, and return rxngenerator that point to batched properly
-    '''
+    final dataset should batch required graphs, get batched features, and return rxngenerator that point to batched properly
+'''
 
 ### first step from raw smiles, generate canon -> mol and reactions with their canon mols
 class DirectSmilesRepo:
@@ -18,30 +19,64 @@ class DirectSmilesRepo:
         # following two should have same keys
         self.canon_to_mol = dict() # canonical smiles -> rdkit mol
         self.r_p_canon = [] # 1 ([reactant canon smiles], [product canon smiles]) for each reaction
+        self.react_broken_bonds = [] # broken bonds list for each reaction, may change to a generic auxillary property object later if going beyond BDEs
 
-    def append_reaction(self, react_smiles, prod_smiles): # one call -> one reaction loaded, and both params are lists
+    def append_reaction(self, react_smiles, prod_smiles, broken_bond_idxs): # one call -> one reaction loaded, and both params are lists
         react_canon = [Chem.CanonSmiles(sm) for sm in react_smiles]
         prod_canon = [Chem.CanonSmiles(sm) for sm in prod_smiles]
 
         self.r_p_canon.append((react_canon, prod_canon))
+        self.react_broken_bonds.append(broken_bond_idxs)
 
         for canon in react_canon + prod_canon:
             if canon in self.canon_to_mol:
                 continue
-            self.canon_to_mol[canon] = Chem.MolFromSmiles(canon)
+            self.canon_to_mol[canon] = Chem.AddHs(Chem.MolFromSmiles(canon))
 
-### generate dgl graphs (non reaction specific) and mappings (reaction specific)
-class DGLwRxnMappings:
+### generate dgl graphs (non reaction specific) and mappings (reaction specific, atom and bond)
+class DGLwBDEMappings:
     def __init__(self, directsmilesrepo):
         self.canon_to_dgl = None
         self.r_p_rxn_mappings = [] # order associated with entries of r_p_canon of entered DirectSmilesRepo
 
+        self.nm = nx_iso.categorical_node_match('specie', 'BAD_MATCH')
         self._load_from_directsmilesrepo(directsmilesrepo)
     
     def _load_from_directsmilesrepo(self, dsr: DirectSmilesRepo): # dsr - direct smiles repo because i hate typing
-        self.canon_to_dgl = {canon : self.dgl_from_mol(mol) for canon, mol in dsr.canon_to_mol.items()}
+        self.canon_to_dgl = {canon : DGLwBDEMappings.dgl_from_mol(mol) for canon, mol in dsr.canon_to_mol.items()}
 
         assert False, 'do your mappings loser, indivudally and as list too(?)'
+
+    @staticmethod
+    def prod_to_reac_atom_map(reac_idx, dsr: DirectSmilesRepo): # assumes single reactant, single bond broken, multiple products, find mapping from prod graph to reactant given reaction idx in dsr
+        reacs, prods = dsr.r_p_canon[reac_idx]
+        reac = reacs[0] # single reactant assumption
+        r_mol = dsr.cannon_to_mol[reac]
+        broken_idx = dsr.react_broken_bonds[reac_idx]
+        p_frags = Chem.GetMolFrags(Chem.FragmentOnBonds(r_mol, broken_idx))
+        r_nx = DGLwBDEMappings.mol_to_nx(r_mol)
+        p_nxs = [r_nx.subgraph(p) for p in p_frags]
+        mols = [dsr.canon_to_mol[canon] for canon in prods]
+        mol_nxs = [DGLwBDEMappings.mol_to_nx(m) for m in mols]
+        '''
+            hi this is me from the future, you suck
+
+            use that trash test you made yesterday to map between atoms on fragments (using p_nxs) and 
+                products generated without fragmenting (mol_nxs), with GraphMatcher
+            \/\/
+            with atom mappings, generate bond mappings using pairs, likely put in a seperate function
+        '''
+        # NOTE: WORK FROM HERE!!
+
+    @staticmethod
+    def mol_to_nx(mol):
+        ng = nx.Graph()
+        ng.add_nodes_from(range(mol.GetNumAtoms()))
+        for b in mol.GetBonds():
+            ng.add_edge(b.GetBeginAtomIdx(), b.GetEndAtomIdx())
+        for a in mol.GetAtoms():
+            ng.nodes[a.GetIdx()]['specie'] = a.GetAtomicNum()
+        return ng
 
     @staticmethod
     def dgl_from_mol(mol):
@@ -51,12 +86,17 @@ class DGLwRxnMappings:
             to_b.append(b.GetIdx())
             from_a.append(b.GetEndAtomIdx())
             to_b.append(b.GetIdx())
+        if mol.GetNumAtoms() == 1: # create fake bond so graph processing will work, from the single atom
+            assert mol.GetBonds() == 0, 'how do you have one atom but non zero bonds???'
+            from_a.append(0)
+            to_b.append(0)
         
         a2b = (torch.tensor(from_a, dtype=torch.int), torch.tensor(to_b, dtype=torch.int))
+        # only one global node, so use zeros to point from single global node to all other nodes
         g2a = (torch.zeros(mol.GetNumAtoms(), dtype=torch.int), torch.arange(mol.GetNumAtoms(), dtype=torch.int))
         g2b = (torch.zeros(mol.GetNumBonds(), dtype=torch.int), torch.arange(mol.GetNumBonds(), dtype=torch.int))
 
-        g = dgl.heterograph({ # reversed tuple are because this graph is bidirectional
+        g = dgl.heterograph({ # reversed tuple for many because this graph is bidirectional
             ('atom', 'a2b', 'bond') : a2b,
             ('bond', 'b2a', 'atom') : tuple(reversed(a2b)),
             ('global', 'g2a', 'atom') : g2a,
--- novel_arch/tests/deep_attn/data.py
+++ novel_arch/tests/deep_attn/data.py
@@ -1,4 +1,8 @@
 from rdkit import Chem
+import dgl
+import networkx.algorithms.isomorphism as nx_iso
+import networkx as nx
+import torch
 
 from novel_arch.deep_attn.data import DirectSmilesRepo, DGLwRxnMappings
 
@@ -8,12 +12,77 @@ from novel_arch.deep_attn.data import DirectSmilesRepo, DGLwRxnMappings
 # print(len(poo.canon_to_mol))
 # print(len(poo.r_p_canon))
 
-sm = '[CH2]N'
-mol = Chem.MolFromSmiles(sm)
-graff = DGLwRxnMappings.dgl_from_mol(mol)
-print(graff)
+# CN(N)CCCC(C)(C)C	3	[CH2]CC(C)(C)C	[CH2]N(C)N
+# COC(=O)OCCC#N	3	CO[C]=O	N#CCC[O]
+# C/C=C/[C@@H](C)CCC#N	17	[H]	C/C=C/[C@@H](C)[CH]CC#N
+# CCC(=O)COC	4	C[O]	[CH2]C(=O)CC
+# C#CCCC(=O)NCCO	2	C#C[CH2]	[CH2]C(=O)NCCO
+# CC[C@@H](CC(C)=O)C(C)C	3	C[C]=O	[CH2][C@H](CC)C(C)C
 
-assert False, "boohoo"
+def mol_to_nx(mol):
+    ng = nx.Graph()
+    ng.add_nodes_from(range(mol.GetNumAtoms()))
+    for b in mol.GetBonds():
+        ng.add_edge(b.GetBeginAtomIdx(), b.GetEndAtomIdx())
+    for a in mol.GetAtoms():
+        ng.nodes[a.GetIdx()]['specie'] = a.GetAtomicNum()
+    return ng
+r = (Chem.MolFromSmiles('CC[C@@H](CC(C)=O)C(C)C'))
+r = Chem.AddHs(r)
+ps = Chem.GetMolFrags(Chem.FragmentOnBonds(r, [3]))
+mols = [(Chem.MolFromSmiles(s)) for s in ['C[C]=O', '[CH2][C@H](CC)C(C)C']]
+mols = [Chem.AddHs(p) for p in mols]
+# print(mol_to_nx(Chem.MolFromSmiles(sm)).nodes.data())
+r = mol_to_nx(r)
+ps = [r.subgraph(p) for p in ps]
+print([p.edges for p in ps])
+mols = [mol_to_nx(m) for m in mols]
+
+nm = nx_iso.categorical_node_match('specie', 'BAD_MATCH')
+gms = [nx_iso.GraphMatcher(m.to_undirected(), p.to_undirected(), nm) for p, m in zip(ps, reversed(mols))]
+print([gm.is_isomorphic() for gm in gms])
+print([gm.mapping for gm in gms])
+
+# def sets_of_keys(sg):
+#     return [set(sg_map.keys()) for sg_map in sg]
+# r_refs = [sets_of_keys(sg) for sg in sg_maps]
+# sg0, sg1 = r_refs
+# joints = 0
+# tot = 0
+# for s0 in sg0:
+#     for s1 in sg1:
+#         tot += 1
+#         if s0.isdisjoint(s1):
+#             joints += 1
+# print('number of joints', joints)
+# print(len(sg0), len(sg1))
+# print(r_refs[1])
+# for i in range(2):
+#     print("pioo", r_refs[0][i] == r_refs[0][i+1])
+
+# for sg in gm.subgraph_isomorphisms_iter():
+#     print(sg)
+# print('total atom count', ps[p_i].number_of_nodes())
+
+exit()
+
+# mol = Chem.MolFromSmiles(sm)
+# ng = nx.Graph()
+# for b in mol.GetBonds():
+#     ng.add_edge(b.GetBeginAtomIdx(), b.GetEndAtomIdx())
+# print('edges wow eh', ng.edges)
+# for a in mol.GetAtoms():
+#     ng.nodes[a.GetIdx()]['specie'] = a.GetSymbol()
+# print(ng.nodes.data())
+
+# params = Chem.SmilesParserParams()
+# params.removeHs = False
+	# 12		C[CH]c1ccnc(C)n1 [H] 'CCc1ccnc(C)n1'
+
+# big_daddy = (Chem.MolFromSmiles('CCc1ccnc(C)n1'))
+# subset = (Chem.MolFromSmarts('C[CH]c1ccnc(C)n1'))
+# matches = big_daddy.GetSubstructMatch(subset)
+# print("wow you", matches)
 
 sm = 'NCCCC(=O)O'
 reac = Chem.MolFromSmiles(sm)
