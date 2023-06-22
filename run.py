86b432f137663715feb2cbeb8a13973ab73fb442
Preet Mistry <pmistry9597@gmail.com>
2023-06-22 17:07:29 -0400

    eval fn + preliminary training loop modify

--- run.py
+++ run.py
@@ -18,64 +18,37 @@ model = DeepAttn(
         graph_inner_layer_sizes=[[64]] * 3,
         residual=True
     )
+from train.eval_metrics import eval_metrics_over_loader
+loader = RxnDataLoader(dset, batch_size=24)
+from sklearn.metrics import mean_absolute_percentage_error, mean_absolute_error
+metric_fns = {'mae': mean_absolute_error, 'mape': mean_absolute_percentage_error}
+print(eval_metrics_over_loader(model, loader, metric_fns, handle_mod_out=lambda x: (x * dset.val_stdev) + dset.val_mean))
 from torch.optim import Adam
 from torch.nn import MSELoss
 loss_fn = MSELoss()
 op = Adam(model.parameters(), lr=0.001)
-print(dset.val_stdev, dset.val_mean)
+# print(dset.val_stdev, dset.val_mean)
 
 for e in range(20):
-    loader = RxnDataLoader(dset, batch_size=64)
+    loader = RxnDataLoader(dset, batch_size=24)
     print('------------- epoch {} ------------'.format(e))
     for i, data in enumerate(loader):
         (graph, feats, feat_gens, idxs), values = data
+        model.train()
         calc = model(graph, feats, feat_gens)
         loss = loss_fn(calc, (values.view([-1, 1]) - dset.val_mean) / dset.val_stdev)
         op.zero_grad()
         loss.backward()
-        # for (n, param) in model.named_parameters():z
+        # for (n, param) in model.named_parameters():
         #     norm = param.grad.norm() if param.grad is not None else None
         #     print('aren\'t you so grad?', n, ': ', norm)
         op.step()
         print('loss lul', loss)
 
+loader = RxnDataLoader(dset, batch_size=24)
+print(eval_metrics_over_loader(model, loader, metric_fns, handle_mod_out=lambda x: (x * dset.val_stdev) + dset.val_mean))
 loader = RxnDataLoader(dset, batch_size=1)
 (graph, feats, feat_gens, idxs), values = next(iter(loader))
-# print('fake', graph, feats, feat_gens, idxs, values)
 model.eval()
 print((model(graph, feats, feat_gens) * dset.val_stdev) + dset.val_mean)
 print(values)
-exit()
-
-# dsr = DirectSmilesRepo()
-# dsr.append_reaction(['NCCCC(=O)O'], ['[CH2]CCC(=O)O', '[NH2]'], [0], 0.2)
-# dsr.append_reaction(['NCCCC(=O)O'], ['[CH2]CC(=O)O', '[CH2]N'], [1], 0.4)
-
-# dsr.append_reaction(['CN(N)CCCC(C)(C)C'], ['[CH2]CC(C)(C)C', '[CH2]N(C)N'], [3], 0.3)
-# dsr.append_reaction(['COC(=O)OCCC#N'], ['CO[C]=O', 'N#CCC[O]'], [3], -0.2)
-# dsr.append_reaction(['C/C=C/[C@@H](C)CCC#N'], ['[H]', 'C/C=C/[C@@H](C)[CH]CC#N'], [17], 0.2)
-
-# bdemap = DGLwBDEMappings(dsr)
-# from novel_arch.deep_attn.data.featurizers import AtomFeaturize, BondFeaturize, GlobalFeaturize
-# aprop = ['atomic_num', 'total_degree', 'total_num_hs', 'ring_of_size', 'is_in_ring']
-# bprop = ['is_in_ring', 'ring_of_size', 'dative']
-# gprop = ['num_atoms', 'num_bonds', 'total_weight']
-# dset = BDEDataset(dsr, bdemap, featurizers={'atom' : AtomFeaturize(aprop, [1, 6, 7, 8]), 'bond' : BondFeaturize(bprop), 'global' : GlobalFeaturize(gprop),}, load_graphs=False)
-
-# this file just makes the import system of python less painful when running scripts w/o a notebook
-
-# import novel_arch.tests.deep_attn.bondnet_sample
-from novel_arch.train_archs import deepattnsum, deepattnattn, deepattnattnnoedges, deepattnglobalattn, bondnet_original
-
-for i in range(4):
-    print('---------------- Trial {} ----------------'.format(i))
-    print('------ deep attn and global attn ------')
-    deepattnglobalattn()
-    print('------ deep attn attn ------')
-    deepattnattn()
-    # print('------ deep attn attn no edges ------')
-    # deepattnattnnoedges()
-    # print('------ deep attn sum ------')
-    # deepattnsum()
-    print('------ bond net original ------')
-    bondnet_original()
---
+++ train/eval_metrics.py
@@ -0,0 +1,18 @@
+# handle items from deep_attn dataloader
+def deep_attn_item_handle(items):
+    dat, val = items
+    return dat[:3], val
+
+## handle items - take raw output from loader, return value and valid model inputs
+def eval_metrics_over_loader(model, loader, metric_fns: dict, handle_items=deep_attn_item_handle, handle_mod_out=lambda x: x):
+    model.eval()
+    metrics = {m_name : [] for m_name in metric_fns}
+    tot_count = 0
+    for items in iter(loader):
+        mod_in, vals = handle_items(items)
+        preds = handle_mod_out(model(*mod_in).detach())
+        item_count = len(vals)
+        for m_name, m_fn in metric_fns.items():
+            metrics[m_name].append(m_fn(vals, preds) * item_count)
+        tot_count += item_count
+    return {m_name : sum(met) / tot_count for m_name, met in metrics.items()}
