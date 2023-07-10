68eca80b18c55820427c97172332ca6514f1f594
Preet Mistry <pmistry9597@gmail.com>
2023-07-10 10:59:12 -0400

    attn params still missing

--- novel_arch/deep_attn/construct_model.py
+++ novel_arch/deep_attn/construct_model.py
@@ -23,14 +23,17 @@ def get_std_model(
 def get_std_sum(b2g_aggregators=bond_sum(), a2g_aggregators=atom_sum(), **kwargs):
     return get_std_model(b2g_aggregators, a2g_aggregators, **kwargs)
 
-def get_attn_model(sum_like=False, graph_inner_layer_sizes=[[64]*3]*4, **kwargs):
+def get_attn_model(sum_like=False,
+            internal_attn_size=16, 
+            graph_hidden_size=32,
+            graph_inner_layer_sizes=[[64]*3]*4, **kwargs):
     graph_layers = len(graph_inner_layer_sizes)
     attn_aggregs = [AtomEdgeReducer(AttnNodeEdgeAggreg(graph_hidden_size, internal_attn_size, sum_like=sum_like))] * graph_layers
     a2g_aggregs = [A2GReducer(AttnNodeEdgeAggreg(graph_hidden_size, internal_attn_size, include_attn_edges=False, sum_like=sum_like))] * graph_layers
     b2g_aggregs = [B2GReducer(AttnNodeEdgeAggreg(graph_hidden_size, internal_attn_size, include_attn_edges=False, sum_like=sum_like))] * graph_layers
     model = get_std_model(
         graph_inner_layer_sizes=graph_inner_layer_sizes,
-        fc_readout_sizes=fc_readout_sizes,
+        # fc_readout_sizes=fc_readout_sizes,
         atom_aggregators=attn_aggregs, 
         b2g_aggregators=b2g_aggregs, 
         a2g_aggregators=a2g_aggregs, 
