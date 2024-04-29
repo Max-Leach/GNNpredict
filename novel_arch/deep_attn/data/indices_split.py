3948978d71d1f16a2ad00aae37fa2fa678453a29
Preet Mistry <pmistry9597@gmail.com>
2024-04-29 15:50:06 -0400

    index splitting try to make little more even split

--- novel_arch/deep_attn/data/indices_split.py
+++ novel_arch/deep_attn/data/indices_split.py
@@ -2,15 +2,18 @@ import csv
 import random
 import copy
 from functools import reduce
+from itertools import chain
 import math
 
 # indices are 0 from the starting point, so by default the line at 1 is considered 0 when compiling indices
-def compile_indices(path, bondtype_col=6, start_line=1):
+def compile_indices(path, bondtype_col=6, start_line=1, max_lines=None):
     with open(path) as f:
         r = csv.reader(f, delimiter=',')
         csv_list = tuple(r)
+        if max_lines == None:
+            max_lines = len(csv_list)
         bt_to_indices = {}
-        for i, rxn_line in enumerate(csv_list[start_line:]):
+        for i, rxn_line in enumerate(csv_list[start_line:max_lines]):
             e = rxn_line[bondtype_col]
             bt = tuple(sorted(e.replace('-','').upper()))
             if bt not in bt_to_indices:
@@ -33,6 +36,7 @@ def split_by_bts(bt_to_indices, set_sizes):
     bt_to_indices = copy.deepcopy(bt_to_indices)
 
     set_indices = [{} for _ in set_sizes]
+    each_size = [0] * len(set_sizes)
     # set_indices = [{}] * len(set_sizes)
     # find portion of bond type list to split and then pop
     for bt, idxs in bt_to_indices.items():
@@ -42,16 +46,18 @@ def split_by_bts(bt_to_indices, set_sizes):
         covered = sum(pop_lens)
         resid = idxs_len - covered
         assert resid < len(pop_lens)
-        # distribute residual until none remaining
-        for i in range(resid):
+        # distribute residual until none remaining among lowest
+        lowest = sorted(list(range(resid)), key=lambda i: each_size[i])
+        for i in lowest:
             pop_lens[i] += 1
         # assert reduce(lambda x,y: x*y, pop_lens) > 0 # check no element is 0
         # print(bt, sum(pop_lens), idxs_len)
         assert sum(pop_lens) == idxs_len # check that split can cover all items
         idxs = idxs.copy()
-        for set_to_bti, pop_len in zip(set_indices, pop_lens):
+        for set_to_bti, pop_len, i in zip(set_indices, pop_lens, range(len(each_size))):
             # pop_len = round(idxs_len * frac)
             set_to_bti[bt] = idxs[-pop_len:]
             idxs = idxs[:-pop_len]
+            each_size[i] += pop_len
     
     return set_indices
