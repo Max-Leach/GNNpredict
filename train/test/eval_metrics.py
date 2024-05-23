88370be355fbfff08257ae865c330cf04f2f613c
Preet Mistry <pmistry9597@gmail.com>
2024-05-23 16:40:43 -0400

    try deleting tensors during test

--- train/test/eval_metrics.py
+++ train/test/eval_metrics.py
@@ -21,4 +21,5 @@ def eval_metrics_over_loader(model, loader, metric_fns: dict, handle_items=deep_
         for m_name, m_fn in metric_fns.items():
             metrics[m_name].append(m_fn(vals.flatten().detach().cpu(), preds.flatten().detach().cpu()) * item_count)
         tot_count += item_count
+        del mod_in, preds, vals
     return {m_name : sum(met) / tot_count for m_name, met in metrics.items()}
