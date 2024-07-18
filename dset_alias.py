9115d0f83642b6c864af9e9fc601a1c93faaba1d
Preet Mistry <pmistry9597@gmail.com>
2024-07-18 11:54:10 -0400

    atomic number list for features adapts to available atoms in dataset

---
+++ dset_alias.py
@@ -0,0 +1,3 @@
+from full_pipeline import dataset_and_split
+
+dataset_and_split.do('/home/moistry/Documents/research/data/combined dset/crap', '/home/moistry/Documents/research/data/combined dset/STUPID_SIMPLE.csv', [0.8, 0.1, 0.1])
--- novel_arch/deep_attn/data/dset_generate.py
+++ novel_arch/deep_attn/data/dset_generate.py
@@ -4,13 +4,16 @@ from novel_arch.deep_attn.data.dataset import BDEDataset
 from novel_arch.deep_attn.data.initial_containers import DirectSmilesRepo, DGLwBDEMappings
 from novel_arch.deep_attn.data.featurizers import AtomFeaturize, BondFeaturize, GlobalFeaturize
 
-def from_csv(path, max_lines=None, start_line=None, entry_name_to_col={'reacs': [1], 'prods': [3,4], 'broken_idx': 2, 'bde': 5}, **kwargs):
+def from_csv(path, max_lines=None, start_line=None, entry_name_to_col={'reacs': [1], 'prods': [3,4], 'broken_idx': 2, 'bde': 5, 'bondtype': 6}, **kwargs):
+    if start_line == None:
+        start_line = 0
+    atomic_num_list = get_atomic_num_list(path, max_lines, start_line, entry_name_to_col['bondtype'])
+    print(atomic_num_list)
+
     with open(path) as csv_file:
         csv_read = csv.reader(csv_file, delimiter=',')
         line_count = 0
         dsr = DirectSmilesRepo()
-        if start_line == None:
-            start_line = 0
         for rxn_line in tuple(csv_read)[start_line:]:
             rsmiles = [rxn_line[i] for i in entry_name_to_col['reacs']]
             psmiles = [rxn_line[i] for i in entry_name_to_col['prods']]
@@ -25,6 +28,22 @@ def from_csv(path, max_lines=None, start_line=None, entry_name_to_col={'reacs': 
         aprop = ['atomic_num', 'total_degree', 'total_num_hs', 'ring_of_size', 'is_in_ring']
         bprop = ['is_in_ring', 'ring_of_size', 'dative']
         gprop = ['num_atoms', 'num_bonds', 'total_weight']
-        dset = BDEDataset.from_initials(dsr, bdemap, featurizers={'atom' : AtomFeaturize(aprop, [1, 6, 7, 8]), 'bond' : BondFeaturize(bprop), 'global' : GlobalFeaturize(gprop),}, **kwargs)
+        dset = BDEDataset.from_initials(dsr, bdemap, featurizers={'atom' : AtomFeaturize(aprop, atomic_num_list), 'bond' : BondFeaturize(bprop), 'global' : GlobalFeaturize(gprop),}, **kwargs)
 
         return dset
+
+from rdkit.Chem import rdchem
+def get_atomic_num_list(path, max_lines, start_line, bond_type_col):
+    atom_types = set()
+    with open(path) as csv_file:
+        csv_read = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for rxn_line in tuple(csv_read)[start_line:]:
+            bt = rxn_line[bond_type_col]
+            bt = bt.split('-')
+            for a in bt:
+                atom_types.add(rdchem.Atom(a).GetAtomicNum())
+            line_count += 1
+            if max_lines != None and line_count >= max_lines:
+                break
+    return sorted(atom_types)
