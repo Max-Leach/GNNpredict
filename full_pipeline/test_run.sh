998ce0578cf86265262808001202e3bd663f0db8
Preet Mistry <pmistry9597@gmail.com>
2024-12-13 13:46:16 -0500

    hpc scripts [should check later if some of these still relevant]

--- full_pipeline/test_run.sh
+++ full_pipeline/test_run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-source ../env/bin/activate
+source ../env/bin/activate # setup python environment
 
 RUN_PATH='/home/moistry/Documents/research/GNNpredict/run.py'
 
 python $RUN_PATH \
         --path '/home/moistry/Documents/research/data/dry_run/model_run' \
-        --dset_path '/home/moistry/Documents/research/data/combined dset/crap/fake_dset/dset' \
-        --train_indices_path '/home/moistry/Documents/research/data/combined dset/crap/train_indices' \
-        --valid_indices_path '/home/moistry/Documents/research/data/combined dset/crap/valid_indices' \
+        --dset_path '/home/moistry/Documents/research/data/dry_run/fake_dset/dset' \
+        --train_indices_path '/home/moistry/Documents/research/data/dry_run/train_indices' \
+        --valid_indices_path '/home/moistry/Documents/research/data/dry_run/valid_indices' \
         --device 'cpu' \
         --num_workers 1 \
         --activation_fn 'silu' \
+        --atom_feat_size 12 \
         \
         --graph_inner_layer_sizes '[[128, 128, 128, 128, 128], [128, 128, 128, 128, 128], [128, 128, 128, 128, 128], [128, 128, 128, 128, 128], [128, 128, 128, 128, 128]]' \
         --graph_hidden_size 64 \
