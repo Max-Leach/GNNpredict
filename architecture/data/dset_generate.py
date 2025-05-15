8f9ba553834e51a71e8741ad5d71e6054d18001a
Preet Mistry <pmistry9597@gmail.com>
2025-05-15 15:07:16 -0400

    dataset encode + train instructions

--- architecture/data/dset_generate.py
+++ architecture/data/dset_generate.py
@@ -7,7 +7,9 @@ from architecture.data.featurizers import AtomFeaturize, BondFeaturize, GlobalFe
 def from_csv(path, max_lines=None, start_line=None, entry_name_to_col={'reacs': [1], 'prods': [3,4], 'broken_idx': 2, 'bde': 5, 'bondtype': 6}, **kwargs):
     if start_line == None:
         start_line = 0
-    atomic_num_list = get_atomic_num_list(path, max_lines, start_line, entry_name_to_col['bondtype'])
+    # atomic_num_list = get_atomic_num_list(path, max_lines, start_line, entry_name_to_col['bondtype'])
+    ## do below so results are more repeatable for users - above version will vary atomic features based on whats available in dataset and if a user only does a subset, the atomic feature size will be different than what deepbde expects
+    atomic_num_list = [1, 5, 6, 7, 8, 9, 14, 15, 16, 17]
 
     with open(path) as csv_file:
         csv_read = csv.reader(csv_file, delimiter=',')
